18bc34f898e80dde848fef8f0f63e07aca8d6565
Everett Kennedy <egkennedy93@gmail.com>
2022-12-23 05:27:32 -0500

    working on course model

--- golf_coordinator/Courses/models.py
+++ golf_coordinator/Courses/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django import template
+from django.urls import reverse
+
 
 # Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=255, unique=False)
+    tee = ''
+
+
+class Golf_Tee(models.Model):
+    course = ''
+    color = ''
+    name = ''
+    par = ''
+    slope = ''
+    rating = ''
+    yardage = ''
+
+
+class Golf_Hole(models.Model):
+    course = ''
+    tee = ''
+    hole_number = ''
+    par = ''
+    yardage = ''
+    Index = ''
