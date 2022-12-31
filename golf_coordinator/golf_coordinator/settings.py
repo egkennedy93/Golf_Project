8811ed28df259e8d0838acf1cc8b9348b5c834b7
Everett Kennedy <egkennedy93@gmail.com>
2022-12-30 22:38:57 -0500

    start work on course list view

--- golf_coordinator/golf_coordinator/settings.py
+++ golf_coordinator/golf_coordinator/settings.py
@@ -121,8 +121,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/images'),
+    os.path.join(BASE_DIR, 'Courses/static')
+]
+
+# MEDIA_ROOT = os.path.join(STATIC_ROOT, 'images')
+# MEDIA_URL = 'images/'
 
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
--- golf_coordinator/golf_coordinator/urls.py
+++ golf_coordinator/golf_coordinator/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf.urls.static import static
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +27,4 @@ urlpatterns = [
     path('courses/', include('Courses.urls', namespace='courses')),
 
 ]
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
