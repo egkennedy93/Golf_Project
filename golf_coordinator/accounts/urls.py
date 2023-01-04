e737644da64d3ccb546c1b864a6d78f24cd4de69
Everett Kennedy <egkennedy93@gmail.com>
2023-01-03 20:40:30 -0500

    account is complete for now

--- golf_coordinator/accounts/urls.py
+++ golf_coordinator/accounts/urls.py
@@ -7,6 +7,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
-    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('signup/', views.register, name='signup'),
+    # path('signup/', views.SignUp.as_view(), name='signup'),
 ]
 
--- golf_coordinator/accounts/views.py
+++ golf_coordinator/accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from accounts.forms import GolferInfoForm, UserForm
 
 from . import forms
 # Create your views here.
@@ -9,3 +10,33 @@ class SignUp(CreateView):
     form_class = forms.GolferInfoForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
+
+def register(request):
+    registered = False
+
+    if request.method == "POST":
+        user_form = UserForm(data=request.POST)
+        golfer_form = GolferInfoForm(data=request.POST)
+
+        if user_form.is_valid() and golfer_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            golfer = golfer_form.save(commit=False)
+            golfer.user = user
+
+            if  'profile_pic' in request.FILES:
+                golfer.picture = request.FILES['picture']
+            
+            golfer.save()
+
+            registered = True
+        else:
+            print(user_form.errors, golfer_form.errors)
+    else:
+        user_form = UserForm()
+        golfer_form = GolferInfoForm()
+    return render(request, 'accounts/signup.html', {'user_form':user_form,
+                                                   'golfer_form': golfer_form,
+                                                   'registered': registered})
