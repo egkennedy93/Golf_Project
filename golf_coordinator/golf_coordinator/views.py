e9c860df33662c86a23e219189374375872904d3
Everett Kennedy <egkennedy93@gmail.com>
2023-01-06 21:56:31 -0500

    updating index

--- golf_coordinator/golf_coordinator/views.py
+++ golf_coordinator/golf_coordinator/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
+from accounts.models import Golfer
 
 class HomePage(TemplateView):
     template_name = 'index.html'
+    extra_context={'golfers': Golfer.objects.all()}
     
