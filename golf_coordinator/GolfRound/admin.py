bb1be3a9718f19f1ac8ec8b0bcad4c33d536edc3
Everett Kennedy <egkennedy93@gmail.com>
2023-01-22 15:58:53 -0500

    round submit is submitting successfully. Need to know clean up UI and account for comparing who won and adding points to teams

--- golf_coordinator/GolfRound/admin.py
+++ golf_coordinator/GolfRound/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from GolfRound.models import Round_Submission, Round_Score
+from GolfRound.models import Round_Score
 
 # Register your models here.
-admin.site.register(Round_Submission)
 admin.site.register(Round_Score)
