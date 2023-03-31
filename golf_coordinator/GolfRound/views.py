bb6b0d033932271c489c0b34854b994125030660
Everett Kennedy <egkennedy93@gmail.com>
2023-03-31 16:42:05 -0400

    made a lot of changes. fixed rt standings updates with scores. made sure scramble scores and matchpaly scores are correct

--- golf_coordinator/GolfRound/views.py
+++ golf_coordinator/GolfRound/views.py
@@ -71,6 +71,7 @@ def RoundSubmissionView(request, teetime_pk):
 
             # Using the round_score_data the two team names are passed (which I need to change to be dynamic), and takes in the gametype for the teetime
             processed_score_data = determine_2v2_team_scores(round_score_data, 'Red', 'Blue', teetime_data.gametype)
+            print(processed_score_data)
         scoreformset.save()
 
         if processed_score_data[2]['net_score'] < 0:
@@ -79,7 +80,8 @@ def RoundSubmissionView(request, teetime_pk):
             winning_score = processed_score_data[2]['net_score'] * -1
 
         elif processed_score_data[2]['net_score'] > 0:
-            team = Trip_Team.objects.get(team=processed_score_data[0][0]['team'].values()[0]['id'])
+            print(processed_score_data[0][0]['team'].values()[0])
+            team = Trip_Team.objects.get(id=processed_score_data[0][0]['team'].values()[0]['id'])
             winning_team = get_object_or_404(Trip_Team, pk=team.id)
             winning_score = processed_score_data[2]['net_score']
 
