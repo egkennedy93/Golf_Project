2c0b8e20c65a9e2f11535db23c4e4f7b876df403
Everett Kennedy <egkennedy93@gmail.com>
2023-04-07 08:00:39 -0400

    getting any stragglers

---
+++ golf_coordinator/bookie/urls.py
@@ -0,0 +1,10 @@
+from bookie import views
+from django.urls import path, include, re_path
+
+app_name = 'bokkie'
+
+urlpatterns = [
+    # url for new course form. currently not being used.
+    path(r'new/', views.CreateCourseView, name='course_new'),
+   
+]
