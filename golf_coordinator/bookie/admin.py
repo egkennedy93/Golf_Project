e42559d0a5bb06f225b851fdb8c9bfc67dcacd8e
Everett Kennedy <egkennedy93@gmail.com>
2023-04-13 19:50:33 -0400

    Have to finish models for bookie

--- golf_coordinator/bookie/admin.py
+++ golf_coordinator/bookie/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from bookie.models import * 
 # Register your models here.
+
+
+
+admin.site.register(GolfBet)
+admin.site.register(PlayerVsPlayer)
+admin.site.register(TeamVsTeam)
--- golf_coordinator/bookie/models.py
+++ golf_coordinator/bookie/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from bookie import models
+from golf_trip.models import * 
 
-# Create your models here.
+
+
+# Model that extends the contrib.auth package to support golfer specific attributes.
+# after developing more, it made more sense to have handicaps associated with the trip, and not the user model. Might have a overall 
+# handicap at a later date 
+
+class GolfBet(models.Model):
+    
+    submitter = models.ForeignKey(Trip_Golfer, on_delete=models.PROTECT)
+    units = models.DecimalField(max_digits=5, decimal_places=2, default=0)
+    
+    
+class PlayerVsPlayer(GolfBet):
+    submitter_tee_time = models.ForeignKey(Trip_TeeTime, related_name='submitter_tee_time', on_delete=models.PROTECT)
+    opponent_tee_time = models.ForeignKey(Trip_TeeTime, related_name='opponent_tee_time', on_delete=models.PROTECT)
+    opponent_bet = models.ForeignKey(Trip_Golfer, on_delete=models.PROTECT)
+
+
+class TeamVsTeam(GolfBet):
+    submitter_tee_time = models.ForeignKey(Trip_TeeTime, related_name='submitter_tee_time', on_delete=models.PROTECT)
+    opponent_tee_time = models.ForeignKey(Trip_TeeTime, related_name='opponent_tee_time', on_delete=models.PROTECT)
+    bet_initiator_team = models.ForeignKey(Trip_Team, related_name='initiator_team', on_delete=models.PROTECT)
+    bet_opponent_team = models.ForeignKey(Trip_Team, related_name='opponet_team', on_delete=models.PROTECT)
