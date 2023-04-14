a8932cdcaa5faa0fb746b196ff28d19d63c8085b
Everett Kennedy <egkennedy93@gmail.com>
2023-04-14 17:25:16 -0400

    added modal to schedule page. Will be used to figure out which form to redirect users to

---
+++ golf_coordinator/bookie/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+from bookie.models import * 
+from golf_trip.models import Trip_TeeTime
+from django.forms import modelformset_factory
--- golf_coordinator/bookie/views.py
+++ golf_coordinator/bookie/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def TeeTimeBetView(request, teetime_pk):
+
+    if request.method == "POST":
+        teetime_data = get_object_or_404(Trip_TeeTime, pk=teetime_pk)
