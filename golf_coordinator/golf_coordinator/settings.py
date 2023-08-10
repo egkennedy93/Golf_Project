e3915ccba567ab660b2b743598d3f4237253403c
Everett Kennedy <egkennedy93@gmail.com>
2023-08-10 09:35:54 -0400

    movign to postgre db

--- golf_coordinator/golf_coordinator/settings.py
+++ golf_coordinator/golf_coordinator/settings.py
@@ -91,10 +91,21 @@ WSGI_APPLICATION = 'golf_coordinator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'golf_coordinator',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
